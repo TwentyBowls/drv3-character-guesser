7ebdd7f14c1e1fb26f5083a4c77a048de5596a6b
reilly <reillyg12@gmail.com>
2022-12-18 14:37:41 -0500

    add drag drop logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,98 +1,105 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './App.css'
 import List from './components/List'
 
 function App() {
-  const [characters, setCharacters] = useState({
-    undecided: ['shuichi', 'kaede'],
-    killers: [],
-    victims: [],
-    survivors: [],
-  })
-
-  function updateCharacters() {
-    let category = null
-    let name = event.target.innerText
-    for (let group in characters) {
-      if(characters[group].includes(name)) {
-        category = group
-        console.log(`we found ${name} in ${group}`)
-      }
-    }
-    console.log('dragged object:')
-    console.log(event.target)
-    setCharacters(prevCharacters => {
-      let oldList = prevCharacters[category]
-      oldList.splice(oldList.indexOf(name), 1)
-      console.log(oldList)
-      console.log(prevCharacters[category])
-      //return {...prevCharacters, [category]: prevCharacters[category]}.filter(currentName => currentName !== name)
-      return {...prevCharacters}
-    })
+  let startingList = {
+      undecided: ['shuichi', 'kaede', 'keebo', 'rantaro'],
+      killers: [],
+      victims: [],
+      survivors: [],
   }
 
-  const handleDragLeave = event => {
-    event.stopPropagation()
-    event.preventDefault()
+  const [characters, setCharacters] = useState(startingList)
+  const dragItem = useRef()
+  const dragOverItem = useRef()
+
+  const dragStart = (e, position) => {
+    //dragItem.current = position
+    dragItem.current = position
+    console.log(`drag start at index ${position} on:`)
+    console.log(e.target.innerHTML)
   }
 
-  const handleDragOver = event => {
-    event.stopPropagation()
-    event.preventDefault()
+  const dragEnter = (e, position) => {
+    //dragOverItem.current = position
+    dragOverItem.current = position
+    console.log(`drag has entered at index ${position} on:`)
+    console.log(e.target)
   }
 
-  const handleDragEnter = event => {
-    event.stopPropagation()
-    event.preventDefault()
+  const drop = (e) => {
+    console.log(`item dropped from index ${dragItem.current} to index ${dragOverItem.current}`)
+    console.log(e.target)
+    // const copyListItems = [...list]
+    // const dragItemContent = copyListItems[dragItem.current]
+    // copyListItems.splice(dragItem.current, 1)
+    // copyListItems.splice(dragOverItem.current, 0, dragItemContent)
+    // dragItem.current = null
+    // dragOverItem.current = null
+    // setList(copyListItems)
   }
 
-  const handleDrop = event => {
-    event.stopPropagation()
-    event.preventDefault()
-    console.log('The bass was dropped')
-    console.log(event.target)
-  }
+  // function updateCharacters() {
+  //   let category = null
+  //   let name = event.target.innerText
+  //   for (let group in characters) {
+  //     if(characters[group].includes(name)) {
+  //       category = group
+  //       console.log(`we found ${name} in ${group}`)
+  //     }
+  //   }
+
+  //   setCharacters(prevCharacters => {
+  //     let oldList = prevCharacters[category]
+  //     oldList.splice(oldList.indexOf(name), 1)
+  //     console.log(oldList)
+  //     console.log(prevCharacters[category])
+  //     //return {...prevCharacters, [category]: prevCharacters[category]}.filter(currentName => currentName !== name)
+  //     return {...prevCharacters}
+  //   })
+  // }
+
+  // const handleDragLeave = event => {
+  //   event.stopPropagation()
+  //   event.preventDefault()
+  // }
 
   return (
     <div className="App">
-      <h1>Sanity Check</h1>
       <section className="characters">
         <List 
           characters={characters.undecided} 
           type='undecided'
-          handleDragLeave={handleDragLeave}
-          handleDragOver={handleDragOver}
-          handleDragEnter={handleDragEnter}
-          updateCharacters={updateCharacters}
-          handleDrop={handleDrop}
+          dragStart={dragStart}
+          dragEnter={dragEnter}
+          drop={drop}
+          idx={100}
           />
         <div className="row roles">
           <List 
             characters={characters.killers} 
             type='killers' 
-            handleDragLeave={handleDragLeave}
-            handleDragOver={handleDragOver}
-            handleDragEnter={handleDragEnter}
-            updateCharacters={updateCharacters}
-            handleDrop={handleDrop}
+            dragStart={dragStart}
+            dragEnter={dragEnter}
+            drop={drop}
+            idx={200}
             />
           <List 
             characters={characters.victims} 
             type='victims' 
-            handleDragLeave={handleDragLeave}
-            handleDragOver={handleDragOver}
-            handleDragEnter={handleDragEnter}
-            updateCharacters={updateCharacters}
-            handleDrop={handleDrop}
+            dragStart={dragStart}
+            dragEnter={dragEnter}
+            drop={drop}
+            idx={300}
             />
           <List 
             characters={characters.survivors} 
             type='survivors' 
-            handleDragLeave={handleDragLeave}
-            handleDragOver={handleDragOver}
-            handleDragEnter={handleDragEnter}
-            updateCharacters={updateCharacters}
-            handleDrop={handleDrop}
+            dragStart={dragStart}
+            dragEnter={dragEnter}
+            drop={drop}
+            idx={400}
             />
         </div>
 
@@ -102,3 +109,64 @@ function App() {
 }
 
 export default App
+
+/*
+import { useState, useRef } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  const [list, setList] = useState(['Item 1','Item 2','Item 3','Item 4','Item 5','Item 6','Item 7','Item 8','Item 9', 'Item 10'])
+  const dragItem = useRef()
+  const dragOverItem = useRef()
+
+  const dragStart = (e, position) => {
+    dragItem.current = position
+    console.log(e.target.innerHTML)
+  }
+
+  const dragEnter = (e, position) => {
+    dragOverItem.current = position
+    console.log(e.target.innerHTMl)
+  }
+
+  const drop = (e) => {
+    const copyListItems = [...list]
+    const dragItemContent = copyListItems[dragItem.current]
+    copyListItems.splice(dragItem.current, 1)
+    copyListItems.splice(dragOverItem.current, 0, dragItemContent)
+    dragItem.current = null
+    dragOverItem.current = null
+    setList(copyListItems)
+  }
+
+  let divs = list.map((item, idx) => {
+    return (<div 
+              className='item'
+              draggable
+              key={idx}
+              onDragStart={(e) => dragStart(e, idx)}
+              onDragEnter={(e) => dragEnter(e, idx)}
+              onDragEnd={drop}
+            >
+                {item}
+            </div>
+    )
+  })
+
+
+  return (
+    <div className="App">
+      <div className="left">
+        {divs}
+      </div>
+      <div className="right"></div>
+    </div>
+  )
+}
+
+export default App
+
+
+*/
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,29 +1,33 @@
 import './List.css'
 import ListHeader from './ListHeader.jsx'
+import {useRef} from 'react'
 
 function List (props) {
     let characters = props.characters.map((character, idx) => {
         return (
-            <h2 
+            <div className='character' 
                 key={idx}
                 draggable='true'
-                //onDragEnd={props.updateCharacters}
+                onDragStart={(e) => props.dragStart(e, idx)}
+                onDragEnd={props.drop}
+                // onDragEnd={console.log('drag end')}
             >
                 {character}
-            </h2>
+            </div>
         )
     })
 
     return (
         <section 
             className={`list characters__${props.type}`}
-            onDragEnter={props.handleDragEnter}
-            onDragOver={props.handleDragOver}
-            onDragExit={props.handleDragLeave}
-            onDrop={props.handleDrop}
         >
             <ListHeader type={props.type} />
-            {characters}
+            <div className="row listContainer"
+            onDragEnter={(e) => props.dragEnter(e, props.idx)}
+            >
+                {characters}
+            </div>
+
         </section>
     )
 }
